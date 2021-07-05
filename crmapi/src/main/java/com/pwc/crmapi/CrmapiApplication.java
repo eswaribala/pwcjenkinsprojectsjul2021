b9d2233809fc8dc2673f71f222c9dc5c783dba8a
eswaribala <parameswaribala@gmail.com>
2021-07-05 11:55:13 +0530

    Added

---
+++ crmapi/src/main/java/com/pwc/crmapi/CrmapiApplication.java
@@ -0,0 +1,13 @@
+package com.pwc.crmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmapiApplication.class, args);
+	}
+
+}
