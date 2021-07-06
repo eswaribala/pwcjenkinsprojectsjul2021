362d5d6d4d6509bd340943b0fa55f44ff6b52d0f
eswaribala <parameswaribala@gmail.com>
2021-07-06 12:19:13 +0530

    updated

--- bankingapi/src/main/java/com/pwc/bankingapi/controllers/CustomerController.java
+++ bankingapi/src/main/java/com/pwc/bankingapi/controllers/CustomerController.java
@@ -1,8 +1,38 @@
 package com.pwc.bankingapi.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pwc.bankingapi.models.Customer;
+import com.pwc.bankingapi.services.CustomerService;
 
 @RestController
 public class CustomerController {
+	@Autowired
+	private CustomerService customerService;
+	
+	@PostMapping("/customers")
+	public ResponseEntity<?> addCustomer(@RequestBody Customer customer){
+		Customer customerObj=this.customerService.addCustomer(customer);
+		if(customerObj!=null) {
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(customerObj);
+		}
+		else
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Customer Not Added");
+		
+		
+	}
+	
+	@GetMapping("/customers")
+	public List<Customer> getAllCustomers(){
+		return this.customerService.getAllCustomers();
+	}
 
 }
